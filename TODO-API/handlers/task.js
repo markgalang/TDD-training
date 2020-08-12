70cb2a479b23d7e675ea2ee32e0346fe37798e35
Mark Galang <galangmarkg@gmail.com>
2020-08-12 15:48:37 +0800

    Add Update and Delete

--- TODO-API/handlers/task.js
+++ TODO-API/handlers/task.js
@@ -1,9 +1,8 @@
 const TASK = require("../models/task");
-const Task = require("../models/task");
 
 const addTask = async (req, res) => {
   try {
-    const TaskData = await Task.create(req.body);
+    const TaskData = await TASK.create(req.body);
     return res.status(201).json(TaskData);
   } catch (err) {
     console.log(err);
@@ -13,7 +12,7 @@ const addTask = async (req, res) => {
 
 const getAllTasks = async (req, res) => {
   try {
-    const allTasks = await Task.find();
+    const allTasks = await TASK.find();
     return res.status(201).json(allTasks);
   } catch (err) {
     console.log(err);
@@ -21,15 +20,45 @@ const getAllTasks = async (req, res) => {
   }
 };
 
-const getTaskById = (req, res) => {
-  res.send("GET TASK BY ID");
+const getTaskById = async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const TaskFound = await TASK.findById(taskId);
+
+    return res.status(200).json(TaskFound);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
-const updateTaskById = (req, res) => {
-  res.send("UPDATE TASK BY ID");
+const updateTaskById = async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const { name } = req.body;
+    const TaskFound = await TASK.findById(taskId);
+
+    TaskFound.name = name;
+    TaskFound.save();
+
+    return res.status(200).json(TaskFound);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
-const deleteTaskById = (req, res) => {
-  res.send("DELETE TASK BY ID");
+const deleteTaskById = async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const TaskFound = await TASK.findById(taskId);
+
+    TaskFound.remove();
+
+    return res.status(204).json(TaskFound);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
 module.exports = {
