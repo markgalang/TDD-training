addc171b556931c23ac033657c41e068f0fb918b
Mark Galang <galangmarkg@gmail.com>
2020-10-22 15:01:10 -0400

    Add unit test on reducer

--- TODO-CLIENT/src/redux/reducers/__tests__/todoReducers.test.js
+++ TODO-CLIENT/src/redux/reducers/__tests__/todoReducers.test.js
@@ -1,9 +1,20 @@
-import todoReducer from "../todoReducer";
-import { ADD_TODO } from "redux/action_types.js";
+import todoReducer from "redux/reducers/todoReducer";
+import { ADD_TODO, GET_ALL_TODOS } from "redux/action_types.js";
 
 let action;
 let newState;
-it("adds new comment to reducers", () => {
+
+it("gets all todos", () => {
+  const mockState = ["TODO1", "TODO2", "TODO3", "TODO4"];
+
+  action = { type: GET_ALL_TODOS, payload: mockState };
+  newState = todoReducer([], action);
+
+  expect(newState.length).toBe(mockState.length);
+  expect(newState).toEqual(mockState);
+});
+
+it("adds new todo to reducers", () => {
   action = { type: ADD_TODO, payload: "New Todo 123" };
 
   newState = todoReducer([], action);
