711b7f8ece57aea3d2ceabcd575bb6fbf2642ba0
Mark Galang <galangmarkg@gmail.com>
2020-10-22 15:25:12 -0400

    Add Reducer unit tests

--- TODO-CLIENT/src/redux/reducers/__tests__/todoReducers.test.js
+++ TODO-CLIENT/src/redux/reducers/__tests__/todoReducers.test.js
@@ -1,22 +1,61 @@
 import todoReducer from "redux/reducers/todoReducer";
-import { ADD_TODO, GET_ALL_TODOS } from "redux/action_types.js";
+import {
+  ADD_TODO,
+  GET_ALL_TODOS,
+  EDIT_TODO,
+  DELETE_TODO,
+} from "redux/action_types.js";
 
 let action;
 let newState;
+let initialState;
 
-it("gets all todos", () => {
-  const mockState = ["TODO1", "TODO2", "TODO3", "TODO4"];
+describe("reducers behavior", () => {
+  beforeEach(() => {
+    initialState = [
+      { _id: 1, title: "TODO1" },
+      { _id: 2, title: "TODO2" },
+      { _id: 3, title: "TODO3" },
+      { _id: 4, title: "TODO4" },
+      { _id: 5, title: "TODO5" },
+      { _id: 6, title: "TODO6" },
+    ];
+  });
+  it("gets all todos", () => {
+    action = { type: GET_ALL_TODOS, payload: initialState };
+    newState = todoReducer([], action);
 
-  action = { type: GET_ALL_TODOS, payload: mockState };
-  newState = todoReducer([], action);
+    expect(newState.length).toBe(initialState.length);
+    expect(newState).toEqual(initialState);
+  });
 
-  expect(newState.length).toBe(mockState.length);
-  expect(newState).toEqual(mockState);
-});
+  it("adds new todo to reducers", () => {
+    action = { type: ADD_TODO, payload: "New Todo 123" };
 
-it("adds new todo to reducers", () => {
-  action = { type: ADD_TODO, payload: "New Todo 123" };
+    newState = todoReducer([], action);
+    expect(newState).toEqual(["New Todo 123"]);
+  });
 
-  newState = todoReducer([], action);
-  expect(newState).toEqual(["New Todo 123"]);
+  it("edit todo info", () => {
+    const todoId = 5;
+    const updatedTodoDetails = { id: todoId, title: "UPDATED TODO ITEM!" };
+
+    action = { type: EDIT_TODO, payload: { todoId, updatedTodoDetails } };
+
+    newState = todoReducer(initialState, action);
+    expect(newState[todoId - 1]).toEqual(updatedTodoDetails);
+  });
+
+  it("deletes todo info", () => {
+    const todoIdToDelete = 5;
+
+    action = { type: DELETE_TODO, payload: { todoIdToDelete } };
+
+    newState = todoReducer(initialState, action);
+    const expectedNewState = initialState.filter(
+      (todo) => todo._id !== todoIdToDelete
+    );
+    expect(newState.length).toBe(expectedNewState.length);
+    expect(newState).toEqual(expectedNewState);
+  });
 });
